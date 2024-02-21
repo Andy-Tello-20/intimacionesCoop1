9ee778d153b5de697e6fd2215d8bd4f85acd1d84
Andrés Tello <andrest911@gmail.com>
2024-02-21 15:56:59 -0300

    se hicieron cambios en el index.js

--- Proyecto1/src/routers/views/index.js
+++ Proyecto1/src/routers/views/index.js
@@ -1,21 +1,47 @@
 import { Router } from 'express';
-
 const router = Router();
+import  { authMiddleware, authRolesMiddleware } from '../../utils.js'
 
-router.get('/create', (req, res) => {
+
+
+
+  
+
+router.get('/', (req, res) => {
+    
+});
+
+router.get('/menuPrincipal',authMiddleware('jwt'), (req, res) => {
+
+    setTimeout(() => {
+        res.render('menuPrincipal', { title: 'Hello People 🖐️' });
+    }, 2000);
+    
+});
+
+router.get('/register', (req, res) => {
+    res.render('register', { title: 'Hello People 🖐️' });
+});
+
+router.get('/login', (req, res) => {
+    res.render('login', { title: 'Hello People 🖐️' });
+});
+
+router.get('/create',authMiddleware('jwt'), authRolesMiddleware('admin'), (req, res) => {
     res.render('create', { title: 'Hello People 🖐️' });
 });
 
-router.get('/getUser', (req, res) => {
+router.get('/getUser',authMiddleware('jwt'), authRolesMiddleware('admin'), (req, res) => {
     res.render('getUser', { title: 'Hello People 🖐️' });
 });
 
-router.get('/UserByLastName', (req, res) => {
+router.get('/UserByLastName',authMiddleware('jwt'), authRolesMiddleware('admin'),(req, res) => {
     res.render('getUserByLastName', { title: 'Hello People 🖐️' });
 });
 
-router.get('/updateUserIndex', (req, res) => {
+router.get('/updateUserIndex',authMiddleware('jwt'), authRolesMiddleware('admin'), (req, res) => {
     res.render('searchUser', { title: 'Hello People 🖐️' });
 });
 
 export default router;
+
